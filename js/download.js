0242fa455383300221306abb57b923bfffa70b02
98WuG <gwu02251250@gmail.com>
2018-07-02 19:16:54 +0000

    loading icons

--- js/download.js
+++ js/download.js
@@ -1,11 +1,13 @@
 function download() {
-	$("#return").text("Processing... (this may take a while)");
+	$("#return").html("<i class=\"fa fa-spinner fa-spin\" style=\"font-size:48px\"></i>");
+	$("#download").html("Download <i class=\"fa fa-circle-o-notch fa-spin ml-2\"></i>");
 	$.ajax({
-			url: "backend/download.php",
-			type: "post",
-			data: { "url" : $("#url").val() },
-			success: function(response) {
-				$("#return").html(response);
-			},
-		});
+		url: "backend/download.php",
+		type: "post",
+		data: { "url" : $("#url").val() },
+		success: function(response) {
+			$("#return").html(response);
+			$("#download").html("Download <i class=\"fa fa-cloud-download ml-2\"></i>");
+		},
+	});
 }
