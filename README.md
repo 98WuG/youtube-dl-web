1a8f504ab0904fa438d17f9c2cdeee7e0926f3b0
98WuG <gwu02251250@gmail.com>
2018-07-02 16:25:55 +0000

    rudimentary working status

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 A "nice-looking" open-source YouTube downloader, powered by `youtube-dl`.
 
-Current status: Not functional.
+Current status: Working
+
+To-do:
+* Catch unsupported URLs
+* Rudimentary loading status
+* Live loading status
+* Auto-setup (cronjob, facl's)
--- download.sh
+++ download.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
 DOWNLOAD_FOLDER="downloads"
-DOWNLOAD_URL="$DOWNLOAD_FOLDER/$(pwgen)"
+DOWNLOAD_URL="$DOWNLOAD_FOLDER/$(pwgen).mp4"
+LOG="ydl.log"
 
-echo "Shell script received \"$@\""
-mkdir -p "$DOWNLOAD_FOLDER"
+echo "Shell script received \"$@\"" >> $DIR/$LOG
+mkdir -p "$DIR/$DOWNLOAD_FOLDER" >> $DIR/$LOG
+setfacl -m u:apache:rwx "$DIR/$DOWNLOAD_FOLDER" >> $DIR/$LOG
+OUTPUT="$(youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' -o "$DIR/$DOWNLOAD_URL" "$@")"
+echo "$OUTPUT" >> $DIR/$LOG
 
-echo "$@" > "$DOWNLOAD_URL"
-
-echo "<br><a href=\"./$DOWNLOAD_URL\">Download</a>"
+echo "<br><a href=\"./$DOWNLOAD_URL\" download>Download</a>"
