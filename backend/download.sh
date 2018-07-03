266de284802200b95133d63d74715deae2e11b8f
98WuG <gwu02251250@gmail.com>
2018-07-03 01:54:04 +0000

    more detailed log (add date)

--- backend/download.sh
+++ backend/download.sh
@@ -5,7 +5,7 @@ DOWNLOAD_FOLDER="downloads"
 DOWNLOAD_URL="$DOWNLOAD_FOLDER/$(pwgen).mp4"
 LOG="ydl.log"
 
-echo "Shell script received \"$@\"" >> $DIR/$LOG
+echo "Shell script received \"$@\" on $(date)" >> $DIR/$LOG
 mkdir -p "$DIR/$DOWNLOAD_FOLDER" >> $DIR/$LOG
 setfacl -m u:apache:rwx "$DIR/$DOWNLOAD_FOLDER" >> $DIR/$LOG
 OUTPUT="$(youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' -o "$DIR/$DOWNLOAD_URL" "$@")"
